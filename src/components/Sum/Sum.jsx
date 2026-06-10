ed9c68ccf724dea4bb0f676f2e9c468f42ea5196
Bruno Rauber <brunorauber.dev@gmail.com>
2026-06-10 15:01:30 -0300

    solution

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,9 +1,3 @@
 export const Sum = ({ a = 0, b = 0 }) => (
-  <>
-    <p>
-      Sum of {a} and {b} is {a + b}
-    </p>
-    {/* Replace paragraphs with Sum componets */}
-    {/* And remove commented lines :) */}
-  </>
+  <p>{`Sum of ${a} and ${b} is ${a + b}`}</p>
 );
